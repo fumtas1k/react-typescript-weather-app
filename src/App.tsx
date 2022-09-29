da1d3086cc3c61933b326cfcb80510e3747aadd9
fumta <takasefumiyuki@team-lab.com>
2022-09-29 12:16:03 +0900

    [change]値をAppで管理

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,58 @@
-import './App.css';
+import { useState } from 'react';
+
 import Title from './components/Title';
 import Form from './components/Form';
+import Results from './components/Results';
+import './App.css';
+
+type ResultsStateType = {
+  country: string;
+  cityName: string;
+  temperature: string;
+  conditionText: string;
+  icon: string;
+}
 
 function App() {
+  const [city, setCity] = useState<string>("");
+  const [results, setResults] = useState<ResultsStateType>({
+    country: "",
+    cityName: "",
+    temperature: "",
+    conditionText: "",
+    icon: "",
+  });
+
+  const baseUrl = process.env.REACT_APP_WEATHER_API_URL;
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+  const getWeather = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    
+    const url = `${baseUrl}v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      setResults({
+        country: data.location.country,
+        cityName: data.location.name,
+        temperature: data.current.temp_c,
+        conditionText: data.current.condition.text,
+        icon: data.current.condition.icon,
+      })
+    });
+  }
+
   return (
-    <div className="App">
-      <Title />
-      <Form />
+    <div className="wrapper">
+      <div className="container">
+        <Title />
+        <Form 
+          setCity={setCity}
+          getWeather={getWeather}        
+        />
+        <Results results={results}/>
+      </div>
     </div>
   );
 }
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,14 @@
-import { useState } from 'react';
+type FormPropsType = {
+  setCity: React.Dispatch<React.SetStateAction<string>>;
+  getWeather: (event: React.FormEvent<HTMLFormElement>) => void;
+}
 
-const Form = () => {
-  const [city, setCity] = useState<string>("");
-  const baseUrl = process.env.REACT_APP_WEATHER_API_URL;
-  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-
-  const getWeather = (event: any) => {
-    event.preventDefault();
-    console.log(baseUrl);
-    
-    const url = `${baseUrl}v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
-    fetch(url)
-    .then(res => res.json())
-    .then(data => console.log(data));
-  }
+const Form = (props: FormPropsType) => {
 
   return (
-    <form action="">
-      <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
-      <button type="submit" onClick={getWeather}>Get Weather</button>
+    <form action="post" onSubmit={props.getWeather}>
+      <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)} />
+      <button type="submit">Get Weather</button>
     </form>
   );
 }
