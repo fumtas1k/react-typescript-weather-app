57dc9ed6eff353cef38aee1b974c5b53cc586371
fumta <takasefumiyuki@team-lab.com>
2022-09-29 11:09:28 +0900

    [add]フォームコンポーネントのロジック追加

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,11 +2,23 @@ import { useState } from 'react';
 
 const Form = () => {
   const [city, setCity] = useState<string>("");
+  const baseUrl = process.env.REACT_APP_WEATHER_API_URL;
+  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
+
+  const getWeather = (event: any) => {
+    event.preventDefault();
+    console.log(baseUrl);
+    
+    const url = `${baseUrl}v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data));
+  }
 
   return (
     <form action="">
       <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWeather}>Get Weather</button>
     </form>
   );
 }
